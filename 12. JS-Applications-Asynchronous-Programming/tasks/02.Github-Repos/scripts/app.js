68f467c66b21392d480dbb41c3ff333f0eefabc1
IvanP <ivanstpv@gmail.com>
2021-02-25 20:03:29 +0200

    before cleanup 2nd task

--- 12. JS-Applications-Asynchronous-Programming/tasks/02.Github-Repos/scripts/app.js
+++ 12. JS-Applications-Asynchronous-Programming/tasks/02.Github-Repos/scripts/app.js
@@ -1,3 +1,16 @@
 function loadRepos() {
-	console.log("TODO...");
+    const username = document.getElementById('username').value;
+    const url = `https://api.github.com/users/${username}/repos`
+    const requestPromise = fetch(url);
+    console.log(requestPromise);
+    requestPromise.then(handleResponse);
+
+    function handleResponse(response) {
+        console.log(response);
+        const dataPromise = response.json();
+        dataPromise.then(handleData)
+    }
+    function handleData(data) {
+        console.log(data);
+    }
 }
