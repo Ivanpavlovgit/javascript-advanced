c8f89785d170ab4839ca9b126339b7d055804427
IvanP <ivanstpv@gmail.com>
2021-03-02 16:07:21 +0200

    2nd task of homework

--- 12. JS-Applications-Asynchronous-Programming/tasks/02.Bus-Schedule/app.js
+++ 12. JS-Applications-Asynchronous-Programming/tasks/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    const busStopInfo = document.getElementById('info');
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+    let stopID = 'depot'
+    let stopName = 'depot';
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        const response = await fetch('http://localhost:3030/jsonstore/bus/schedule/' + stopID);
+        const data = await response.json();
+
+        stopName=data.name;
+        stopID = data.next;
+
+        arriveButton.disabled = false;
+        departButton.disabled = true;
+
+        busStopInfo.textContent = `Next stop ${stopName}`;
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+
+        busStopInfo.textContent = `Arriving at ${stopName}`;
     }
 
     return {
