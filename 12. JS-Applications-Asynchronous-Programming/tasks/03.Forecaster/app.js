3f60aca5d44d3fb5c773bea7318550a20d98babe
IvanP <ivanstpv@gmail.com>
2021-03-02 17:36:15 +0200

    3rd task semi finished logic

--- 12. JS-Applications-Asynchronous-Programming/tasks/03.Forecaster/app.js
+++ 12. JS-Applications-Asynchronous-Programming/tasks/03.Forecaster/app.js
@@ -1,5 +1,24 @@
 function attachEvents() {
-    console.log("TODO...");
+    const locationArea = document.querySelector("#location");
+    const weatherButton = document.getElementById('submit');
+    let locationCode = '';
+
+
+    weatherButton.addEventListener('click', getForecast);
+
+    async function getForecast() {
+        const response = await fetch('http://localhost:3030/jsonstore/forecaster/locations');
+        const data = await response.json();
+        Object.values(data).forEach(e => {
+            if (e.name === locationArea.value)
+                locationCode = e.code;
+        });
+        console.log(locationCode);
+        const dayResponse = await fetch('http://localhost:3030/jsonstore/forecaster/today/' + locationCode);
+        const dayData = await dayResponse.json();
+        console.log(dayData)
+    }
+
 }
 
 attachEvents();
