a60b0a2ca94247f0282240ba6514d6e603127ea1
IvanP <ivanstpv@gmail.com>
2021-03-04 22:00:41 +0200

    cookbook made as SPA with modules

--- 14. JS-Applications-Single-Page-Applications/lab/js-apps-workshop-master/lesson-04/base/src/create.js
+++ 14. JS-Applications-Single-Page-Applications/lab/js-apps-workshop-master/lesson-04/base/src/create.js
@@ -1,40 +1,60 @@
-const form = document.querySelector('form');
+import { showDetails } from './details.js';
 
-form.addEventListener('submit', (ev => {
-    ev.preventDefault();
-    const formData = new FormData(ev.target);
-    onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
-}));
 
-async function onSubmit(data) {
-    const body = JSON.stringify({
-        name: data.name,
-        img: data.img,
-        ingredients: data.ingredients.split('\n').map(l => l.trim()).filter(l => l != ''),
-        steps: data.steps.split('\n').map(l => l.trim()).filter(l => l != '')
-    });
+let main;
+let section;
+let setActiveNav;
 
-    const token = sessionStorage.getItem('authToken');
-    if (token == null) {
-        return window.location.pathname = 'index.html';
-    }
+export function setupCreate(targetMain, targetSection, onActiveNav) {
+    main = targetMain;
+    section = targetSection;
+    setActiveNav = onActiveNav;
+    const form = targetSection.querySelector('form');
 
-    try {
-        const response = await fetch('http://localhost:3030/data/recipes', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Authorization': token
-            },
-            body
+    form.addEventListener('submit', (ev => {
+        ev.preventDefault();
+        const formData = new FormData(ev.target);
+        onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
+    }));
+
+    async function onSubmit(data) {
+        const body = JSON.stringify({
+            name: data.name,
+            img: data.img,
+            ingredients: data.ingredients.split('\n').map(l => l.trim()).filter(l => l != ''),
+            steps: data.steps.split('\n').map(l => l.trim()).filter(l => l != '')
         });
-        
-        if (response.status == 200) {
-            window.location.pathname = 'index.html';
-        } else {
-            throw new Error(await response.json());
+
+        const token = sessionStorage.getItem('authToken');
+        if (token == null) {
+            return alert('You\'re not logged in!');
         }
-    } catch (err) {
-        console.error(err.message);
+
+        try {
+            const response = await fetch('http://localhost:3030/data/recipes', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Authorization': token
+                },
+                body
+            });
+
+            if (response.status == 200) {
+                showDetails((await response.json())._id);
+            } else {
+                const error = await response.json();
+                throw new Error(error.message);
+            }
+        } catch (err) {
+            alert(err.message);
+            console.error(err.message);
+        }
     }
+}
+
+export function showCreate() {
+    setActiveNav('createLink');
+    main.innerHTML = '';
+    main.appendChild(section);
 }
--- 14. JS-Applications-Single-Page-Applications/lab/js-apps-workshop-master/lesson-04/base/src/login.js
+++ 14. JS-Applications-Single-Page-Applications/lab/js-apps-workshop-master/lesson-04/base/src/login.js
@@ -1,33 +1,57 @@
-const form = document.querySelector('form');
+import { showCatalog } from './catalog.js';
 
-form.addEventListener('submit', (ev => {
-    ev.preventDefault();
-    const formData = new FormData(ev.target);
-    onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
-}));
 
-async function onSubmit(data) {
-    const body = JSON.stringify({
-        email: data.email,
-        password: data.password,
-    });
+let main;
+let section;
+let setActiveNav;
 
-    try {
-        const response = await fetch('http://localhost:3030/users/login', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body
+export function setupLogin(targetMain, targetSection, onActiveNav) {
+    main = targetMain;
+    section = targetSection;
+    setActiveNav = onActiveNav;
+
+    const form = targetSection.querySelector('form');
+
+    form.addEventListener('submit', (ev => {
+        ev.preventDefault();
+        const formData = new FormData(ev.target);
+        onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
+    }));
+
+    async function onSubmit(data) {
+        const body = JSON.stringify({
+            email: data.email,
+            password: data.password,
         });
-        const data = await response.json();
-        if (response.status == 200) {
-            sessionStorage.setItem('authToken', data.accessToken);
-            window.location.pathname = 'index.html';
-        } else {
-            throw new Error(data.message);
+
+        try {
+            const response = await fetch('http://localhost:3030/users/login', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body
+            });
+            const data = await response.json();
+            if (response.status == 200) {
+                sessionStorage.setItem('authToken', data.accessToken);
+                sessionStorage.setItem('userId', data._id);
+                document.getElementById('user').style.display = 'inline-block';
+                document.getElementById('guest').style.display = 'none';
+                
+                showCatalog();
+            } else {
+                alert(data.message);
+                throw new Error(data.message);
+            }
+        } catch (err) {
+            console.error(err.message);
         }
-    } catch (err) {
-        console.error(err.message);
     }
+}
+
+export function showLogin() {
+    setActiveNav('loginLink');
+    main.innerHTML = '';
+    main.appendChild(section);
 }
