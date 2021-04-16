258c2ac96d9154a0c8a09664e78828ec12389d87
IvanP <ivanstpv@gmail.com>
2021-04-16 14:03:32 +0300

    router demo with page.js page.redirect

--- 17. JS-Applications-Routing/lab/pageDemo/pDemo.js
+++ 17. JS-Applications-Routing/lab/pageDemo/pDemo.js
@@ -4,19 +4,41 @@ const pages = {
     '/home': '<h2>Home Page</h2> <p> home Content</p>',
     '/catalog': '<h2>Catalog Page</h2> <p> catalog Content</p>',
     '/catalog/123': '<h2>Catalog Page 123</h2> <p> catalog Content 123</p>',
+    '/catalog/124': '<h2>Catalog Page 124</h2> <p> catalog Content 124</p>',
+    '/catalog/pesho/12': '<h2>Catalog Page 125</h2> <p> catalog Content 125</p>',
+    '/catalog/mesho/14': '<h2>Catalog Page 125</h2> <p> catalog Content 125</p>',
     '/about': '<h2>About Page</h2> <p> about Content</p>',
     '/about/5': '<h2>About Page</h2> <p> about Content</p>'
 }
+
 const defaultPage = '<h2>404</h2> <p> Page not found :D!</p>'
 const main = document.querySelector('main');
 
-page('/home', () => updateContent('/home'));
-page('/catalog', () => updateContent('/catalog'));
-page('/catalog/123', () => updateContent('/catalog/123'));
-page('/about', () => updateContent('/about'));
-page('/about/5', () => updateContent('/about/5'));
+page('/home', updateContent);
+page('/catalog', updateContent);
+page('/catalog/:myName/:myAge', itemDetails);
+page('/catalog/:id', updateContent);
+page('/about', updateContent);
+page('/about/5', updateContent);
+page.redirect('/', '/home');
 page.start();
 
-function updateContent(pathname) {
-    main.innerHTML = pages[pathname] || defaultPage;
+function updateContent(context) {
+    //  console.log(context);
+    main.innerHTML = pages[context.pathname] || defaultPage;
+}
+
+function itemDetails(context) {
+    const myName = context.params.myName;
+    const myAge = context.params.myAge;
+    const html = `<h2>Item ${myName}</h2><p>Details for ${myName} is ${myAge} years old.</p>`
+    main.innerHTML = html;
+
+    const btn = document.createElement('button');
+    btn.textContent=`buy ${myName}`
+    btn.addEventListener('click', () => {
+        context.page.redirect('/catalog');
+        console.log(context);
+    })
+    main.appendChild(btn);
 }
